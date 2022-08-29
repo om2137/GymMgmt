b8f3d030faff6730c949300138c454d1ceb6d4d2
om2137 <omraut961@gmail.com>
2022-08-30 00:34:18 +0530

    base url added

--- next.config.js
+++ next.config.js
@@ -3,5 +3,6 @@ module.exports = {
   reactStrictMode: true,
   env: {
     MONGO_URI: "mongodb+srv://GymAdmin:Sandy4888@gym.8i0oneq.mongodb.net/MembersDatabase?retryWrites=true&w=majority",
+    BASE_URL: "http://localhost:3000",
   }
 }
