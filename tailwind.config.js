9269863c5eb6ad6c84453961889fbf3fd5d222df
om2137 <omraut961@gmail.com>
2022-09-03 15:44:18 +0530

    reverting changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-    // Add more here
-],
+  
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
