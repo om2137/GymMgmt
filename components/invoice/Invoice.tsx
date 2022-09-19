06297d30522f6f0728a31bb66ed2fb319d4e36cf
om2137 <omraut961@gmail.com>
2022-09-19 23:15:54 +0530

    first draft invoice

--- components/invoice/Invoice.tsx
+++ components/invoice/Invoice.tsx
@@ -13,13 +13,14 @@ type Props = {
     first: string;
     middle: string;
     last: string;
-    // address: string;
+    address: string;
     // phone: number;
     // birthdate: string;
     // age: number;
-    // gender: string;
+    gender: string;
     // mstatus: string;
     // id: number;
+    fees : string;
 }
 
 const burl = process.env.BASE_URL_ENV;
@@ -33,9 +34,8 @@ export async function getStaticProps(context: any) {
     }
   }
 
-const InvoiceForm: React.FC<Props> = ( {title,first,middle,last} ) => {
+const InvoiceForm: React.FC<Props> = ( {title,first,middle,last,address,gender,fees} ) => {
     console.log("call");
-    const [startDate, setStartDate] = useState(new Date());
 
     const handlePrint = () => {
         window.print();
@@ -71,19 +71,89 @@ const InvoiceForm: React.FC<Props> = ( {title,first,middle,last} ) => {
       }
 
     return(
-        <main className=' flex items-center justify-center  '>
-            <div className='border-2 border-gray-400 py-10 rounded bg-white px-20 '>
+        <main className=' flex flex-col items-center justify-center '>
+            <div className="p-4">
+                <Button label="print" onClick={handlePrint} className="bg-green-500 hover:bg-green-400 px-3"/>
+            </div>
+            
+            <div className='w-[45rem] border-2 border-gray-400 py-10 rounded bg-white px-20 '>
                 <div>
-                    <h2 className='my-6 text-center text-4xl font-bold text-gray-900'>{title}</h2>
-                    <form className='py-6' onSubmit={handleForm} >
-                        <div className='rounded-md shadow-sm  text-left'>
+                    <div className='my-4'>
+                        <div className="flex justify-center">
+                            <h2 className=' text-4xl font-bold text-gray-900 p-2'>Sandy's fitness care</h2>
+                            <span className="font-semibold text-center text-xl m-auto">Recipt no:</span>
+                        </div>
+                        
+                        <div className="flex text-sm p-2">
+                            <div className='w-2/3 '>
+                                <h3 className=' text-center px-4'>
+                                <span className="font-semibold">Address: </span>
+                                Mohopada, Behind Shivsena shakha, Tal. Khalapur, Dist. Raigad.
+                            </h3>
+                            </div>
+                            <div className='w-1/3 px-2'>
+                                <h2 className='font-semibold ' >
+                                    M: 1234567890 <br/>
+                                    M: 1234567890
+                                </h2> 
+                            </div>
+                        
+                        </div> 
+                    </div>
+                    
+                    <h3 className="pl-6">
+                        {/* {
+                            gender === "male" ? <div>
+                                <span className='font-semibold text-2xl '>Mr. {first+" "+middle+" "+last}</span>
+                            </div> : 
+                            <div>
+                                <span className='font-semibold text-2xl '>Mrs. {first+" "+middle+" "+last}</span>
+                            </div>
+                        } */}
+                        <span className='font-semibold text-2xl '>Mr. Sandeep Patil</span>
+                    </h3>
+
+                    <div className='p-4 ml-4'>
+                        <span className="font-semibold">
+                            A sum of Rupees (in words) : {fees}
+                        </span> 
+                    </div>
+                    
+                    <div className='flex'>
+                        <div className=' flex font-bold px-6 w-2/3'>
+                            <div className='px-4'>
+                                <span>
+                                    Facility 
+                                </span>
+                            </div>
+                            <div className='px-4'>
+                                <span>
+                                    Due Date  
+                                </span>
+                            </div>
+                            <div className='px-4'>
+                                <span>
+                                    Amount 
+                                </span>
+                            </div>
+                        </div>
+                        <div className='w-1/3 font-bold'>
+                            <span>
+                                Sign: 
+                            </span>
+                        </div>
+                    </div>
+                    
+
+                     <form className='' onSubmit={handleForm} >
+                         <div className='rounded-md shadow-sm  text-left'>
                             {/* start */}
-                            <div className=''>
+                             {/* <div className=''>
                                 <div className='flex pb-3'>
                                     <div>
                                         <div className="mb-3 xl:w-48 pr-6">
-                                            <label className=''>Name:</label>
-                                            <label htmlFor="">{first+" "+middle+" "+last}</label>
+                                            <label className=''>Name:</label><br/>
+                                            <label htmlFor="">{first+" "+middle+" "+last}Temperory name</label>
                                         </div>
 
                                         <div>
@@ -107,7 +177,7 @@ const InvoiceForm: React.FC<Props> = ( {title,first,middle,last} ) => {
                                                 border-2 border-gray-300 border-dashed cursor-pointer dark:hover:bg-bray-800  hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-200">
                                                 <div className="flex flex-col justify-center items-center pt-5 pb-6">
                                                     <svg className="mb-3 w-10 h-6 text-gray-400" fill="file" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path></svg>
-                                                    {/* <p className="mb-2 text-xs text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>a */}
+                                                  
                                                     <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
                                                 </div>
                                                 <input id="dropzone-file" type="file" className="hidden" />
@@ -158,13 +228,14 @@ const InvoiceForm: React.FC<Props> = ( {title,first,middle,last} ) => {
                             </div>
                             <div className='py-4 text-center'>
                                 <Button label="Add" type="submit" className="bg-red-500 hover:bg-red-400 px-3"/>
-                            </div>
+                            </div> */}
                             {/* end */}
                         </div> 
-                    </form>
-                    <Button label="print" onClick={handlePrint} className="bg-green-500 hover:bg-green-400 px-3"/>
+                    </form> 
+                    
                 </div>
             </div>
+
         </main>
         
     )
