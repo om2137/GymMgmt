b09369fe193d219cdd0b0b0514e6d300fc7b3dc5
om2137 <omraut961@gmail.com>
2022-09-24 22:57:41 +0530

    all invoice database page

--- pages/invoice.tsx
+++ pages/invoice.tsx
@@ -1,19 +1,72 @@
-import React from 'react'
+import type { NextPage } from 'next'
+import Head from 'next/head'
 import Navbar from '../components/Navbar'
-import InvoiceForm from '../components/invoice/Invoice'
+const axios = require('axios').default;
+import MediaCard from '../components/cards'
+import baseUrl from '../helper/baseUrl';
 
+export async function getServerSideProps(context: any) {
+  
+  const res = await axios(`${baseUrl}/api/invoice`);
+  const {invoice} = res.data;
+  return {
+    props: {
+      invoices : invoice,
+    },
+  }
+}
 
-export default function invoice() {
+const invoice: NextPage = ({invoices }:any) => {
+  
   return (
     <>
-        {/* <Navbar title='Invoice'/> */}
-        <main>
-            <div className='p-10'>
-                <InvoiceForm  first='' middle='' last='' gender='' fees='' inNum={100}/>
-            </div>
-            
-        </main>
-        
+    <Navbar 
+      title="Profiles"
+    />
+    <main className='px-6'>
+      
+      {/* modal */}
+      <div>
+        {/* <User/> */}
+        <div className='flex flex-col justify-center sm:justify-start'>
+          {
+            invoices.map((invoice : any) => {
+              
+              return (
+                <div className=' p-5'>
+                  <div className='flex text-black'>
+                    <div className="flex px-4">
+                        <span className="font-semibold ">number: </span> 
+                        {invoice.invoiceNumber}
+                    </div>
+                    <div className="px-4">
+                      <span className="font-semibold">Name: </span> 
+                      {invoice.Name}
+                    </div>
+                    <div className="px-4">
+                      <span className="font-semibold">Paid: </span> 
+                      {invoice.paidDate}
+                    </div>
+                    <div className="px-4">
+                      <span className="font-semibold">Due: </span> 
+                      {invoice.dueDate}
+                    </div>
+                    <div className="px-4">
+                      <span className="font-semibold">Fees: </span> 
+                      {invoice.fees}
+                    </div>
+                  </div>
+                </div>
+                
+              )
+            })
+          }  
+        </div>
+      </div>
+    </main>
+    
     </>
   )
 }
+
+export default invoice
