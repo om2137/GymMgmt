6e36467dffc015f6d12c9ac91b18613ba4fa5dbe
om2137 <omraut961@gmail.com>
2022-09-24 23:08:05 +0530

    profile page update

--- pages/profiles.tsx
+++ pages/profiles.tsx
@@ -44,22 +44,37 @@ const profiles: NextPage = ({members}:any) => {
                 const month = birth.getMonth()+1;
                 const day = birth.getDate();
                 const year = birth.getFullYear();
+
+                
+              const payDate = new Date(member.paidDate);
+              const payMonth = payDate.getMonth()+1;
+              const payDay = payDate.getDate();
+              const payYear = payDate.getFullYear();
+
+              const dueDate = new Date(member.dueDate);
+              const dueMonth = dueDate.getMonth()+1;
+              const dueDay = dueDate.getDate();
+              const dueYear = dueDate.getFullYear();
+
                 return (
                   
                   <div className='p-5 '>
                     <div className=' text-black'>
                     <MediaCard
-                        image={member.Avatar}
-                        first={member.Firstname}
-                        middle={member.Middlename}
-                        last={member.Lastname}
-                        address={member.Address}
-                        phone={member.Contact}
-                        Dob={day + '/' + month + '/' + year}
-                        age={age}
-                        gender={member.Gender}
-                        marriage={member.Mstat} id={0}                      
-                      />
+                      image={member.Avatar}
+                      first={member.Firstname}
+                      middle={member.Middlename}
+                      last={member.Lastname}
+                      address={member.Address}
+                      phone={member.Contact}
+                      Dob={day+'/'+month+'/'+year}
+                      age={age}
+                      gender={member.Gender}
+                      marriage={member.Mstat}
+                      id={member._id}
+                      paid={payDay+'/'+payMonth+'/'+payYear}
+                      due={dueDay+'/'+dueMonth+'/'+dueYear}
+                    />
                     </div>
                   </div>
                   
