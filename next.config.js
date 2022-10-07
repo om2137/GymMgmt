921eed806e201e3b03fef9a034ded1881aa5f5cb
om2137 <omraut961@gmail.com>
2022-10-07 23:34:00 +0530

    temp token

--- next.config.js
+++ next.config.js
@@ -3,6 +3,6 @@ module.exports = {
   reactStrictMode: true,
   env: {    
     CLOUD_URI: "https://api.cloudinary.com/v1_1/dqpsoptzm/image/upload",
-    ACCESS_TOKEN: 'EAAGDG3uEhHkBAIZBnRcAh2R42zR9B35uQ9ozNkNJX4BR8qkX2aCUNGA5vHLSFZBWmmLgSdCzsGmMvWgABuNPgfztTgAhMxFGZCs70SnZAHGewRIdORZBsc9IlAtaDn8EXHCZCUuBvZA6Dktxtc6ghmKQ70dJS9D7ZCgCvotJQaZC9EvZBqKQcShCIl2ZBhu3YKzLzO1yv4BRj3RvwJMfSdfmkTsX8QnfVnMPnEZD'
+    ACCESS_TOKEN: 'EAAGDG3uEhHkBAMZCvsx1qbViDsesnpiEoSnaem8G316LZC1NdULSgsTZBipEbapnglM6TmeWVnmbMWvfLixrqvVwtZC1lNrKmEFu9gZCZBPU2xTJTfnKzmqc662yMfwzGWm3vzFKlQnDl2IwT4Hf5OsJxBaKEHZBnH2rbZCkX0C2WxngPnY1MHJHQ05UpP6CGFCW3qZBTZCWb1uVd8RY3mtk6edifMbEsZAuwwZD'
   }
 }
