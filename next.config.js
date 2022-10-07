d0e34b174e9f200ddc551febb0b74c8cdb6cf678
om2137 <omraut961@gmail.com>
2022-10-07 23:11:11 +0530

    whatsapp cloud api key

--- next.config.js
+++ next.config.js
@@ -3,5 +3,6 @@ module.exports = {
   reactStrictMode: true,
   env: {    
     CLOUD_URI: "https://api.cloudinary.com/v1_1/dqpsoptzm/image/upload",
+    ACCESS_TOKEN: 'EAAGDG3uEhHkBAIZBnRcAh2R42zR9B35uQ9ozNkNJX4BR8qkX2aCUNGA5vHLSFZBWmmLgSdCzsGmMvWgABuNPgfztTgAhMxFGZCs70SnZAHGewRIdORZBsc9IlAtaDn8EXHCZCUuBvZA6Dktxtc6ghmKQ70dJS9D7ZCgCvotJQaZC9EvZBqKQcShCIl2ZBhu3YKzLzO1yv4BRj3RvwJMfSdfmkTsX8QnfVnMPnEZD'
   }
 }
