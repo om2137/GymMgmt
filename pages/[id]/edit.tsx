37ec878b3f270687616cd392374b8aa97dc595bc
om2137 <omraut961@gmail.com>
2022-10-18 23:37:11 +0530

    editing admission date

--- pages/[id]/edit.tsx
+++ pages/[id]/edit.tsx
@@ -39,7 +39,7 @@ const EditForm: React.FC<Props> = ( {members}:any ) => {
         Gender: members.Gender,
         Mstat: members.Mstat,
         Avatar: members.Avatar,
-        AdmissionDate: members.admissionDate,
+        admissionDate: members.admissionDate,
     })
     
     const handleChange = (e: React.ChangeEvent<any>) => {
@@ -141,10 +141,11 @@ const EditForm: React.FC<Props> = ( {members}:any ) => {
                         /><span className=''>Date of Admission: </span>
                         <input type="date" autoComplete='none'  
                             onChange={handleChange} 
-                            name='admissionDate' value={form.AdmissionDate}
+                            name='admissionDate' value={form.admissionDate}
                             className='py-2 rounded relative block w-full px-3 
                             border border-gray-600 placeholder-gray-500 text-gray-900 mb-2
-                            focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm'placeholder='Admission Date' 
+                            focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm'
+                            placeholder='Admission Date' 
                         />
                         {/*  <input type="file"  /> */}
                         <div className='pt-2'> 
