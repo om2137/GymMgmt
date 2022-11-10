1298070ecb849a439dbd6d5730a5d619f9f85987
om2137 <omraut961@gmail.com>
2022-11-10 19:08:55 +0530

    preserving scroll position added integrated in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
+import { usePreserveScroll } from '../components/usePreserveScroll'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const preserveScroll = usePreserveScroll();
   return(
     <SessionProvider session={pageProps.session}>
-      <Component {...pageProps} />
+      <Component {...pageProps} preservescroll={preserveScroll} />
     </SessionProvider>
   ) 
   
