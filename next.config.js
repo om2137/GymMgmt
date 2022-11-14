12385c42fc6fbd7fed0ecd3c42917b80d8750698
om2137 <omraut961@gmail.com>
2022-11-15 00:08:27 +0530

    env variable shifted

--- next.config.js
+++ next.config.js
@@ -2,7 +2,5 @@
 module.exports = {
   reactStrictMode: true,
   env: {    
-    CLOUD_URI: "https://api.cloudinary.com/v1_1/dqpsoptzm/image/upload",
-    ACCESS_TOKEN: 'EAAGDG3uEhHkBAMZCvsx1qbViDsesnpiEoSnaem8G316LZC1NdULSgsTZBipEbapnglM6TmeWVnmbMWvfLixrqvVwtZC1lNrKmEFu9gZCZBPU2xTJTfnKzmqc662yMfwzGWm3vzFKlQnDl2IwT4Hf5OsJxBaKEHZBnH2rbZCkX0C2WxngPnY1MHJHQ05UpP6CGFCW3qZBTZCWb1uVd8RY3mtk6edifMbEsZAuwwZD'
-  }
+    }
 }
