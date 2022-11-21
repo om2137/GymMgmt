355e800e915ce711410c6bbccefa6ed9cd9335f9
om2137 <omraut961@gmail.com>
2022-11-21 13:39:24 +0530

    home page layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Navbar from '../components/Navbar'
 const axios = require('axios').default;
 import MediaCard from '../components/cards'
 import baseUrl from '../helper/baseUrl';
+import SavePdf from '../components/SavePdf'
 
 export async function getServerSideProps(context: any) {
   
@@ -26,17 +27,29 @@ const Home: NextPage = ({members,invoices}:any) => {
     <Navbar 
       title="Profiles"
     />
-    <main className='px-6'>
+    <main className='px-6 md:mt-8'>
       
       {/* modal */}
       <div>
-        <div className='flex justify-center font-semibold text-4xl p-6 align-center'>
+        <div className='flex justify-center font-semibold text-4xl p-6 m-4 text-center'>
           Welcome to the Dashboard of Sandys Fitness Care
         </div>
-        <div className='flex flex-col md:flex-row justify-center text-center p-5'>
+        <div className='flex flex-col justify-center text-center p-5'>
+          <span className='text-2xl md:text-4xl font-semibold p-4'>Overview</span>
+          <div className='flex flex-col capitalize justify-center'>
+            <div className='p-2'>
+              <span className='font-semibold'>total members: </span>  {members.length}
+            </div>
+            <div className='p-2'>
+            <span className='font-semibold'> total invoices: </span>{invoices.length}
+            </div>
+          </div>
+        </div>
+        <div className='flex flex-col sm:flex-row justify-center items-center p-5'>
+            
             <div className='md:p-6 p-3 '>
               <a
-                  className="inline-block border border-gray-500 text-sm px-6 py-3 mr-2 rounded  mt-4 lg:inline-block lg:mt-0 font-bold"
+                  className="inline-block border border-gray-500 text-sm px-6 py-3 rounded  mt-4 lg:inline-block lg:mt-0 font-bold"
                   href="../profiles"
                 >
                   Profiles
@@ -44,7 +57,7 @@ const Home: NextPage = ({members,invoices}:any) => {
             </div>
             <div className='md:p-6 p-3'>
               <a
-                  className="inline-block border border-gray-500 text-sm px-6 py-3 mr-2 rounded  mt-4 lg:inline-block lg:mt-0 font-bold"
+                  className="inline-block border border-gray-500 text-sm px-6 py-3 rounded  mt-4 lg:inline-block lg:mt-0 font-bold"
                   href="../invoice"
                 >
                   Invoices
@@ -52,31 +65,30 @@ const Home: NextPage = ({members,invoices}:any) => {
             </div>
             <div className='md:p-6 p-3'>
               <a
-                  className="inline-block border border-gray-500 text-sm px-6 py-3 mr-2 rounded  mt-4 lg:inline-block lg:mt-0 font-bold"
+                  className="inline-block border border-gray-500 text-sm px-6 py-3  rounded  mt-4 lg:inline-block lg:mt-0 font-bold"
                   href="../admission"
                 >
                   Admission
                 </a>
             </div>
+            
         </div>
-        <div className='flex flex-col justify-center text-center p-5'>
-          <span className='text-2xl font-semibold'>Overview</span>
-          <div className='flex flex-col justify-center'>
-            <div className=''>
-              total members: {members.length}
-            </div>
-            <div className=''>
-              total invoices: {invoices.length}
-            </div>
-          </div>
-        </div>
+        
 
         {/* <User/> */}
       </div>
     </main>
+    <div className='text-2xl font-bold py-10'>
+        <footer>
+          <h2>
+            
+          </h2>
+        </footer>
+    </div>
     
     </>
   )
+  
 }
 
 export default Home
