03afb6e6de35130c08b81c3f39b6a34a81678eb5
om2137 <omraut961@gmail.com>
2022-12-06 23:25:53 +0530

    /invoice/index (invoice database). new invoice layout

--- pages/invoices/[id]/index.tsx
+++ pages/invoices/[id]/index.tsx
@@ -18,17 +18,16 @@ export async function getServerSideProps(context: any) {
   }
 
 export default function EachInvoice({invoices}: any) {
-    
-    const payDate = new Date(invoices.paidDate);
-    const payMonth = payDate.getMonth()+1;
-    const payDay = payDate.getDate();
-    const payYear = payDate.getFullYear();
-
-    const dueDate = new Date(invoices.dueDate);
-    const dueMonth = dueDate.getMonth()+1;
-    const dueDay = dueDate.getDate();
-    const dueYear = dueDate.getFullYear();
-    const due = dueDate.toDateString();
+    const name = invoices.Name ;
+    const gender = invoices.Gender;
+    const fee = invoices.fees;
+    const facility = invoices.facility;
+    const paid = invoices.paidDate;
+    const due = invoices.dueDate;
+    const phone = invoices.Contact;
+    const paidon = invoices.paidOn;
+    const admFee =invoices.admFee;
+    const total = (Number(fee)) + (Number(admFee)) ;
 
     const feesWord = converter.toWords(invoices.fees);
 
@@ -65,11 +64,10 @@ export default function EachInvoice({invoices}: any) {
             </div>
             
             
-            <div className='w-[45rem] border shadow-2xl border-black py-10  bg-white px-16 '>
+            {/* <div className='w-[45rem] border shadow-2xl border-black py-10  bg-white px-16 '>
                 <div>
                     <div className='my-4'>
                         <div className="flex justify-center align-end">
-                            {/* <h2 className=' text-4xl font-bold text-gray-900 p-2'>Sandy's fitness care</h2> */}
                             <div className=''>
                                <img src="../../Gym_assets/mainlogo.jpg" alt="" className='w-[24rem] content-center m-4 '/> 
                             </div>
@@ -100,14 +98,7 @@ export default function EachInvoice({invoices}: any) {
                     </div>
                     
                     <h3 className="pl-6 text-center font-bold capitalize">
-                        {/* {
-                            invoices.gender === "male" ? <div>
-                                <span className=' text-2xl '>Mr. {invoices.Name}</span>
-                            </div> : 
-                            <div>
-                                <span className=' text-2xl '>Mrs. {invoices.Name}</span>
-                            </div>
-                        } */}
+                        
                         <span className=' text-2xl '>Mr. {invoices.Name}</span>
                     </h3>
 
@@ -130,7 +121,7 @@ export default function EachInvoice({invoices}: any) {
                                         Facility 
                                     </span>
                                     <span className='capitalize'>
-                                        {/* {facility} */}
+                                        
                                         cardio
                                     </span>
                                 </div>
@@ -148,7 +139,7 @@ export default function EachInvoice({invoices}: any) {
                                     </span>
                                     <span>
                                         {invoices.fees}
-                                        {/* 500 */}
+                                        
                                     </span>
                                 </div>
                             </div>
@@ -170,8 +161,139 @@ export default function EachInvoice({invoices}: any) {
                     </div>
                     
                 </div>
-            </div>
+            </div> */}
             
+            <div className='w-[45rem] ml:w-[66rem]  border-2 border-gray-400 py-10 rounded bg-white px-20 ml:px-28 ml:py-14' id='PDF'>
+                <div>
+                    <div className='my-4'>
+                        <div className="flex justify-center align-end">
+                            <div className=''>
+                               <img src="../../Gym_assets/mainlogo2.webp" alt="" className='w-[25rem] ml:w-[37rem] content-center m-2 '/> 
+                            </div>
+                            
+                            <div className="font-semibold text-center ml:text-2xl text-xl ml:pl-6 m-auto">
+                                <div className='py-1 ml:py-2'>
+                                    <span className="font-semibold ml:text-xl text-base ">Recipt no: </span>
+                                    <span className="font-normal ">{invoices.invoiceNumber}</span>
+                                </div>
+                                <div className='py-1 ml:py-2'>
+                                    <span className="font-semibold ml:text-xl text-base">Paid on: </span>
+                                    <span className="font-normal ml:text-lg text-sm">{invoices.paidOn}</span>
+                                </div>
+                            </div>
+                            
+                        </div>
+                        
+                        <div className="flex text-sm p-2 ml:text-xl">
+                            <div className='w-2/3 '>
+                                <h3 className=' text-center px-4'>
+                                <span className="font-semibold">Address: </span>
+                                Mohopada, Behind Shivsena shakha, Tal. Khalapur, Dist. Raigad.
+                            </h3>
+                            </div>
+                            <div className='w-1/3 px-2'>
+                                <h2 className='font-semibold pl-14 ml:pl-28' >
+                                    M: 8888053456 <br/>
+                                    M: 7350034888
+                                </h2> 
+                            </div>
+                        
+                        </div> 
+                    </div>
+                    
+                    <h3 className="pl-6  text-center font-bold capitalize">
+                        {
+                            gender === "male" ? <div>
+                                <span className=' text-2xl ml:text-4xl '>Mr. {name}</span>
+                            </div> : 
+                            <div>
+                                <span className=' text-2xl ml:text-4xl '>Mrs. {name}</span>
+                            </div>
+                        }
+                    </h3>
+                    
+                    <div className='flex item-center'>
+                        <div className='flex justify-center ml:text-2xl p-2 ml:p-4 ml-8 '>
+                            <span className="text-center font-bold capitalize">
+                                A sum of Rupees  <br />
+                                (in words)  
+                            </span> 
+                            <span className="text-center font-semibold capitalize p-2">
+                                : {feesWord}  Rupees
+                            </span>
+                        </div>
+                    </div>
+                    
+                    
+                    <div className='flex p-2'>
+                        
+                        <div className=' flex flex-col w-[70rem] ml:text-xl'>
+                            <div className="flex">
+                                <div className='flex flex-col px-4 '>
+                                    <span className='font-bold'>
+                                        Facility 
+                                    </span>
+                                    <span className=' capitalize'>
+                                        {facility}
+                                        {/* cardio */}
+                                    </span>
+                                </div>
+                                <div  className='flex flex-col px-4'>
+                                    <span  className='font-bold'>
+                                        Start 
+                                    </span>
+                                    <span>
+                                        {paid}
+                                        {/* 23/2/2021 */}
+                                    </span>
+                                </div>
+                                <div  className='flex flex-col px-4'>
+                                    <span  className='font-bold'>
+                                        Due
+                                    </span>
+                                    <span>
+                                        {due}
+                                        {/* 23/2/2021 */}
+                                    </span>
+                                </div>
+                                <div  className='flex flex-col px-4 '>
+                                    <span  className='font-bold'>
+                                        Adm.fee 
+                                    </span>
+                                    <span>
+                                        {admFee}
+                                    </span>
+                                </div>
+                                <div  className='flex flex-col px-4'>
+                                    <span  className='font-bold'>
+                                        Amount 
+                                    </span>
+                                    <span>
+                                        {fee}
+                                        {/* 500 */}
+                                    </span>
+                                </div>
+                            </div>
+                            
+                            <div className='text-center font-bold p-4'>
+                                <span>
+                                    Total Amount : {total}
+                                </span>
+                            </div>
+                        </div>
+                        <div className='w-[35rem] text-xl ml:text-3xl font-bold text-center '>
+                            <span>
+                                Sign <br /> 
+                            </span>
+                            <span className='flex justify-center'>
+                                <img src="../../Gym_assets/signature.svg" className='w-28 pt-2 lg:pt-4' alt="" />
+                            </span>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+
         </main>
         
     )
