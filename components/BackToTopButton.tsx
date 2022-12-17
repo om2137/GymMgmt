6fdf3aa3a3adfc0e0c650c27e4a191b066b50f8a
om2137 <omraut961@gmail.com>
2022-12-17 23:20:18 +0530

    backToTop component active

--- components/BackToTopButton.tsx
+++ components/BackToTopButton.tsx
@@ -1,7 +1,44 @@
-import React from 'react'
+import {useEffect, useState} from 'react'
+import Button from "./Button";
 
 export default function BackToTopButton() {
+
+  const [backToTop, setBackToTop] = useState(false);
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 100) {
+        setBackToTop(true);
+      } else {
+        setBackToTop(false);
+      }
+    });
+  }, [])
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
   return (
-    <div>BackToTopButton</div>
+    <>
+      {backToTop && (
+          <button
+              style={{
+                position: 'fixed', 
+                bottom: '40px', 
+                right: '40px',
+                height: '40px',
+                width: '40px',
+              }}
+              className="bg-gray-500 hover:bg-gray-400 px-3 text-white uppercase py-2 rounded font-2xl text-sm select-none"
+              onClick={scrollToTop}
+          >
+              ^
+          </button>
+        )
+      }
+    </>
+    
   )
 }
