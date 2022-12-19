a7843905d65e230b1b97751ee8f778e8d2a80108
om2137 <omraut961@gmail.com>
2022-12-19 23:24:41 +0530

    search bar results

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -8,6 +8,17 @@ export default function SearchBar() {
                 className="w-[600px] px-5 py-3 text-lg rounded-full border-2 border-gray-500  focus:border-gray-700 outline-none transition"
                 placeholder="Search"
             />
+            {/* search results */}
+            <div 
+                className="w-[580px] absolute mt-1 w-full text-lg p-2 bg-white rounded-bl rounded-br max-h-36 shadow-xl overflow-y-auto"
+            >
+                <ul>
+                    <li>test</li>
+                    <li>test</li>
+                    <li>test</li>
+                    <li>test</li>
+                </ul>
+            </div>
         </div>
     </div>
   )
