d48cbbb5699e94e786e23cffb54353e5d9706e98
om2137 <omraut961@gmail.com>
2022-12-20 13:32:04 +0530

    searching from database working

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,27 +1,32 @@
-import { FC } from 'react'
 
-interface Props {
-
+interface Props<T> {
+    results?: T[];
+    renderItem: (item: T) => JSX.Element;
+    onChange?: React.ChangeEventHandler;
 }
 
-const SearchBar: FC<Props> = (props): JSX.Element => {
+const SearchBar = <T extends object>({results =[], renderItem,onChange}: Props<T>): JSX.Element => {
   return (
     <div className='flex items-center justify-center'>
         <div>
             <input type="text" 
+                onChange={onChange}
                 className="w-[600px] px-5 py-3 text-lg rounded-full border-2 border-gray-500  focus:border-gray-700 outline-none transition"
                 placeholder="Search"
             />
             {/* search results */}
             <div 
-                className="w-[580px] absolute mt-1 w-full text-lg p-2 bg-white rounded-bl rounded-br max-h-36 shadow-xl overflow-y-auto"
+                className="w-[580px]  absolute mt-1 w-full text-lg p-2 bg-white rounded-bl rounded-br max-h-44 shadow-xl overflow-y-auto"
             >
-                <ul>
-                    <li>test</li>
-                    <li>test</li>
-                    <li>test</li>
-                    <li>test</li>
-                </ul>
+                {
+                    results.map((item, index) => {
+                        return (
+                            <div key={index} className="courso-pointer rounded hover:bg-slate-400 hover:bg-opicity-10 p-2">
+                                {renderItem(item)}
+                            </div>
+                        )
+                    })
+                }
             </div>
         </div>
     </div>
