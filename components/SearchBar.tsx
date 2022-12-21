b2accb34648aa450041036acdf02bde4a8cd5ea3
om2137 <omraut961@gmail.com>
2022-12-21 18:00:37 +0530

    searchbar sizing

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -27,13 +27,13 @@ const SearchBar = <T extends object>({results =[], renderItem,onChange}: Props<T
             {/* search results */}
             {showRes && 
                 <div 
-                    className="lg:w-[400px] xl:w-[580px]  absolute mt-1 w-full text-lg p-2 bg-white rounded-bl rounded-br max-h-44 shadow-xl overflow-y-auto"
+                    className="lg:w-[400px] xl:w-[580px]  absolute mt-1 w-full text-lg p-2 bg-white rounded-bl rounded-br max-h-64 shadow-xl overflow-y-auto"
                 >
                     {
                         results.map((item, index) => {
                             return (
-                                <div key={index} className="courso-pointer rounded hover:bg-slate-400 hover:bg-opicity-10 p-2" >
-                                    {renderItem(item)} {index}
+                                <div key={index} className="flex courso-pointer rounded hover:bg-slate-400 hover:bg-opicity-10 p-2" >
+                                    {renderItem(item)} 
                                 </div>
                             )
                         })
@@ -45,19 +45,19 @@ const SearchBar = <T extends object>({results =[], renderItem,onChange}: Props<T
         <div className="lg:hidden ">
             <input type="text" 
                 onChange={onChange}
-                className="md:w-[168px] ml:w-[250px] px-5 py-3 text-lg rounded-full border-2 border-gray-500  focus:border-gray-700 outline-none transition"
+                className="w-[168px] ml:w-[250px] px-5 py-3 text-lg rounded-full border-2 border-gray-500  focus:border-gray-700 outline-none transition"
                 placeholder="Search"
             />
             {/* search results */}
             {showRes && 
                 <div 
-                    className="w-[200px] absolute mt-1 w-full text-lg p-2 bg-white rounded-bl rounded-br max-h-44 shadow-xl overflow-y-auto"
+                    className="w-[200px] ml:w-[250px] absolute mt-1 w-full text-lg p-2 bg-white rounded-bl rounded-br max-h-44 shadow-xl overflow-y-auto"
                 >
                     {
                         results.map((item, index) => {
                             return (
                                 <div key={index} className="courso-pointer rounded hover:bg-slate-400 hover:bg-opicity-10 p-2">
-                                    {renderItem(item)} {index}
+                                    {renderItem(item)} 
                                 </div>
                             )
                         })
