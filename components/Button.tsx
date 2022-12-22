7ae356b8748d4e22775f69d1d1a5cf30a2308561
om2137 <omraut961@gmail.com>
2022-12-22 23:08:27 +0530

    button animation

--- components/Button.tsx
+++ components/Button.tsx
@@ -8,7 +8,7 @@ type Props = {
 const Button: React.FC<Props> = ({label, onClick, className,typeIn}) =>{
     return(
         <button
-            className={className+" text-white uppercase py-2 rounded font-normal text-sm select-none"}
+            className={className+" text-white uppercase py-2 rounded font-normal text-sm select-none transition transform hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none "}
             onClick={onClick}
             type={typeIn}
         >
