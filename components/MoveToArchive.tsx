8e01bfaa036510875f19c23312a6bc3fa2045054
om2137 <omraut961@gmail.com>
2023-01-13 00:01:40 +0530

    move to archive button initiated

---
+++ components/MoveToArchive.tsx
@@ -0,0 +1,15 @@
+import {useEffect, useState} from 'react'
+
+export default function MoveToArchive() {
+
+  return (
+    <>
+        <button
+            className="bg-red-500 hover:bg-white hover:text-red-500 border border-red-500 text-white uppercase py-3 px-3 rounded font-semibold select-none"
+        >
+            Move to Archive
+        </button>
+    </>
+    
+  )
+}
