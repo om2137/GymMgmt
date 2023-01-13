d39b2cd3ec5df4be9df611398832b40a2126aa29
om2137 <omraut961@gmail.com>
2023-01-13 23:39:08 +0530

    search bar redirect to profile

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,14 +1,65 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 interface Props<T> {
     results?: T[];
     renderItem: (item: T) => JSX.Element;
     onChange?: React.ChangeEventHandler;
+    onSelect?: (item: T) => void;
+    value?: string;
 }
 
-const SearchBar = <T extends object>({results =[], renderItem,onChange}: Props<T>): JSX.Element => {
+const SearchBar = <T extends object>({results =[], renderItem,onChange,onSelect,value}: Props<T>): JSX.Element => {
     // result contianer visibility handler
     const [showRes,setShowRes] = useState(false);
+    const resultContainer = useRef<HTMLDivElement>(null);
+    const [focusedIndex, setFocusedIndex] = useState(-1);
+    const [defaultValue, setDefaultValue] = useState("");
+
+    const handleSelection = (selectedIndex: number) => {
+        const selectedItem = results[selectedIndex];
+        if(!selectedItem) return resetSearchComplete();
+        onSelect && onSelect(selectedItem);
+        resetSearchComplete();
+    };
+
+    const resetSearchComplete = useCallback(() => {
+        setShowRes(false);
+    }, [])
+
+    const handleKeyDown: React.KeyboardEventHandler<HTMLDivElement> = (e) => {
+        const { key } = e;
+        let nextIndexCount = 0;
+    
+        // move down
+        if (key === "ArrowDown")
+          nextIndexCount = (focusedIndex + 1) % results.length;
+    
+        // move up
+        if (key === "ArrowUp")
+          nextIndexCount = (focusedIndex + results.length - 1) % results.length;
+    
+        // hide search results
+        if (key === "Escape") {
+          resetSearchComplete();
+        }
+    
+        // select the current item
+        if (key === "Enter") {
+          e.preventDefault();
+          handleSelection(focusedIndex);
+        }
+    
+        setFocusedIndex(nextIndexCount);
+    };
+
+    useEffect(() => {
+        if (!resultContainer.current) return;
+
+        resultContainer.current.scrollIntoView({
+            block: "center",
+        });
+    }, [focusedIndex]);
+
     useEffect(() => {
         if(results.length > 0){
             setShowRes(true);
@@ -18,8 +69,9 @@ const SearchBar = <T extends object>({results =[], renderItem,onChange}: Props<T
     }, [results]);
   return (
     <div className='flex items-center justify-center'>
-        <div className="hidden lg:inline ">
-            <input type="text" 
+        <div tabIndex={1} onKeyDown={handleKeyDown} className="hidden lg:inline ">
+            <input 
+                type="text" 
                 onChange={onChange}
                 className="lg:w-[420px] xl:w-[600px] px-5 py-3 text-lg rounded-2xl border focus:border-2 border-gray-500  focus:border-gray-700 outline-none transition"
                 placeholder="Search"
@@ -32,7 +84,13 @@ const SearchBar = <T extends object>({results =[], renderItem,onChange}: Props<T
                     {
                         results.map((item, index) => {
                             return (
-                                <div key={index} className="flex courso-pointer rounded hover:bg-slate-400 hover:bg-opicity-10 p-2" >
+                                <div 
+                                    key={index} 
+                                    ref={index === focusedIndex ? resultContainer : null}
+                                    style={{backgroundColor: index === focusedIndex  ? "#bebebe" : ""}}
+                                    onMouseDown={() => handleSelection(index)}
+                                    className="flex courso-pointer rounded hover:bg-slate-400 hover:bg-opicity-10 p-2" 
+                                >
                                     {renderItem(item)} 
                                 </div>
                             )
@@ -42,7 +100,7 @@ const SearchBar = <T extends object>({results =[], renderItem,onChange}: Props<T
             }
             
         </div>
-        <div className="lg:hidden ">
+        <div  tabIndex={1} onKeyDown={handleKeyDown} className="lg:hidden ">
             <input type="text" 
                 onChange={onChange}
                 className="w-[168px] ml:w-[250px] px-5 py-3 text-lg rounded-2xl border focus:border-2 border-gray-500  focus:border-gray-700 outline-none transition"
@@ -56,7 +114,13 @@ const SearchBar = <T extends object>({results =[], renderItem,onChange}: Props<T
                     {
                         results.map((item, index) => {
                             return (
-                                <div key={index} className="courso-pointer rounded hover:bg-slate-400 hover:bg-opicity-10 p-2">
+                                <div 
+                                    key={index} 
+                                    ref={index === focusedIndex ? resultContainer : null}
+                                    style={{backgroundColor: index === focusedIndex  ? "#bebebe" : ""}}
+                                    onMouseDown={() => handleSelection(index)}
+                                    className="courso-pointer rounded hover:bg-slate-400 hover:bg-opicity-10 p-2"
+                                >
                                     {renderItem(item)} 
                                 </div>
                             )
