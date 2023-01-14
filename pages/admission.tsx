49a00fb3e28385505ac75b3a435040112b884d34
om2137 <omraut961@gmail.com>
2023-01-14 13:50:27 +0530

    redeployment

--- pages/admission.tsx
+++ pages/admission.tsx
@@ -6,7 +6,7 @@ import Testsign from '../components/Testsign'
 const axios = require('axios').default;
 import baseUrl from '../helper/baseUrl';
 
-export async function getStaticProps(context: any) {
+export async function getServerSideProps(context: any) {
   
   const res = await axios(`${baseUrl}/api/member`);
   // console.log(res.data.member);
