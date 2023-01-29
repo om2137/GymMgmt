0ac09afcb76a46354877b47f54d0fc5c7642fc77
om2137 <omraut961@gmail.com>
2023-01-29 23:22:10 +0530

    button ui improved, handle delete added to moveToArchive component

--- components/MoveToArchive.tsx
+++ components/MoveToArchive.tsx
@@ -1,14 +1,33 @@
 import {useEffect, useState} from 'react'
+const axios = require('axios').default;
+import Router from 'next/router';
+import baseUrl from '../helper/baseUrl';
 
-export default function MoveToArchive() {
+interface props{
+  id:number;
+}
+
+export default function MoveToArchive({id}:props) {
+
+  const handleDelete = async() => {
+    try{
+        const deleteMember = await axios( `${baseUrl}/api/member/${id}` , {
+          method: "DELETE",
+        });
+        Router.push('/')
+    }catch(err){
+      console.log(err);
+    }
+  }
 
   return (
     <>
-        <button
-            className="bg-red-500 hover:bg-white hover:text-red-500 border border-red-500 text-white uppercase py-3 px-3 rounded font-semibold select-none"
-        >
-            Move to Archive
-        </button>
+      <button
+        className="bg-red-500 hover:bg-red-400 px-3 text-white uppercase py-2 rounded font-normal text-sm select-none transition transform hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none"
+        onClick={handleDelete}
+      >
+        Move to Archive
+      </button>
     </>
     
   )
