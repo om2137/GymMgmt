708e8c8146a7d76a6ac532f13e1ee4fee88efbd5
om2137 <omraut961@gmail.com>
2023-02-20 23:57:17 +0530

    active section api

---
+++ pages/api/active/[id].js
@@ -0,0 +1,55 @@
+import dbConnect from "../../../db/dbconnect";
+import Active from "../../../models/Active";
+
+dbConnect();
+
+//unique record: get, edit, delete
+
+export default async (req, res) => {
+    const { 
+        query: { id },
+        method 
+    } = req;
+    
+    switch (method) {
+        case "GET":
+            try{
+                const active = await Active.findById(id);
+                if(!active){
+                    res.status(404).json({success:false, message:"active not found"});
+                }
+                res.status(200).json({success:true, active});
+            }catch(err){
+                res.status(400).json({success:false})
+            }
+            break;
+        case "PUT":
+            try{
+                const active = await Active.findByIdAndUpdate(id, req.body, {
+                    new:true,
+                    runValidators:true
+                });
+                if(!active){
+                    res.status(404).json({success:false, message:"active not found"});
+                }
+                res.status(200).json({success:true, active});
+            }catch(err){
+                res.status(400).json({success:false, err})
+            }
+            break;
+        case "DELETE":
+            try{
+                const active = await Active.deleteOne({_id:id});
+                if(!active){
+                    res.status(404).json({success:false, message:"active not found"});
+                }
+                res.status(200).json({success:true, archive});
+            }catch(err){
+                res.status(400).json({success:false})
+            }
+            break;
+        default:
+            res.status(405).json({success:false, message: "Method not allowed" });
+            break;
+    }
+}
---
+++ pages/api/active/index.js
@@ -0,0 +1,35 @@
+import dbConnect from "../../../db/dbconnect";
+import Active from "../../../models/Active";
+
+dbConnect();
+
+// get, post records
+
+export default async (req, res) => {
+  const { method } = req;
+
+  switch (method) {
+    case "GET":
+      try{
+        const actives = await Active.find({});
+        res.status(200).json({success:true, active:actives});
+      }catch(err){
+        res.status(400).json({success:false})
+      }
+      break;
+
+    case "POST":
+      try{
+        const active = await Active.create(req.body);
+        res.status(200).json({success:true, active:archive});
+      }catch(err){
+        res.status(400).json({success:false+err+"text"})
+        console.log("error:om "+err);
+      }
+      break;
+
+    default:
+      res.status(405).json({success:false, message: "Method not allowed" });
+      break;
+  }
+}
