0176c8d2ad0b3225ea037c6d886fcc05c1ba61f1
om2137 <omraut961@gmail.com>
2023-02-21 23:57:56 +0530

    archive section api

---
+++ pages/api/archive/[id].js
@@ -0,0 +1,55 @@
+import dbConnect from "../../../db/dbconnect";
+import Archival from "../../../models/Archival";
+
+dbConnect();
+
+//unique record: get, edit, delete
+
+export default async (req, res) => {
+    const { 
+        query: { id },
+        method 
+    } = req;
+    
+    switch (method) {
+        case "GET":
+            try{
+                const archive = await Archival.findById(id);
+                if(!archive){
+                    res.status(404).json({success:false, message:"archive not found"});
+                }
+                res.status(200).json({success:true, archive});
+            }catch(err){
+                res.status(400).json({success:false})
+            }
+            break;
+        case "PUT":
+            try{
+                const archive = await Archival.findByIdAndUpdate(id, req.body, {
+                    new:true,
+                    runValidators:true
+                });
+                if(!archive){
+                    res.status(404).json({success:false, message:"archive not found"});
+                }
+                res.status(200).json({success:true, archive});
+            }catch(err){
+                res.status(400).json({success:false, err})
+            }
+            break;
+        case "DELETE":
+            try{
+                const archive = await Archival.deleteOne({_id:id});
+                if(!archive){
+                    res.status(404).json({success:false, message:"archive not found"});
+                }
+                res.status(200).json({success:true, archive});
+            }catch(err){
+                res.status(400).json({success:false})
+            }
+            break;
+        default:
+            res.status(405).json({success:false, message: "Method not allowed" });
+            break;
+    }
+}
---
+++ pages/api/archive/index.js
@@ -0,0 +1,35 @@
+import dbConnect from "../../../db/dbconnect";
+import Archival from "../../../models/Archival";
+
+dbConnect();
+
+// get, post records
+
+export default async (req, res) => {
+  const { method } = req;
+
+  switch (method) {
+    case "GET":
+      try{
+        const archives = await Archival.find({});
+        res.status(200).json({success:true, archive:archives});
+      }catch(err){
+        res.status(400).json({success:false})
+      }
+      break;
+
+    case "POST":
+      try{
+        const archive = await Archival.create(req.body);
+        res.status(200).json({success:true, archive:archive});
+      }catch(err){
+        res.status(400).json({success:false+err+"text"})
+        console.log("error:om "+err);
+      }
+      break;
+
+    default:
+      res.status(405).json({success:false, message: "Method not allowed" });
+      break;
+  }
+}
