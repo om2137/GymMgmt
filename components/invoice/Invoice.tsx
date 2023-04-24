857d04b3432d1e9d878dc9cf9933249d787824d4
om2137 <omraut961@gmail.com>
2023-04-24 23:47:58 +0530

    ios buttion

--- components/invoice/Invoice.tsx
+++ components/invoice/Invoice.tsx
@@ -95,7 +95,7 @@ const InvoiceForm: React.FC<Props> = ( {first,middle,last,gender,fees,paid,due,f
                 <div className="p-4">
                     <SendPdf rootElementID="PDF" phone={phone} />
                 </div>
-                <div className="p-4 hidden">
+                <div className="p-4 ">
                     <SendIOS rootElementID="PDF" phone={phone} />
                 </div>
                 <div className="p-4">
